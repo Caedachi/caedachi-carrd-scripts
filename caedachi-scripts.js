a672006963db0d50f43f9f7cfcac2b30a503417a
Caedachi <cae+github@caedachi.art>
2021-08-28 12:20:38 -0400

    query for commission status

--- caedachi-scripts.js
+++ caedachi-scripts.js
@@ -1,5 +1,5 @@
 (function() {
-  let owo = [
+  var owo = [
      "    ,o888888o.  `8.`888b                 ,8'  ,o888888o.",
      " . 8888     `88. `8.`888b               ,8'. 8888     `88.",
      ",8 8888       `8b `8.`888b             ,8',8 8888       `8b",
@@ -12,20 +12,40 @@
      "    `8888888P'           `8.`   `8'           `8888888P'"
   ];
 
-  let title = "• WELCOME TO THE SHARK ZONE ".replaceAll(' ', '\xa0');
+  var title = '• WELCOME TO THE SHARK ZONE '.replaceAll(' ', '\xa0');
+  var commissionTextId = 'text09';
 
-  function main() {
+  function scrollTitle() {
     title = title.slice(2) + title.slice(0, 2);
     document.title = title;
   }
+
+  function responseListener() {
+    let commissionText = document.getElementById(commissionTextId);
+    let response = JSON.parse(this.responseText);
+    let status = response.desc.toLowerCase();
+    commissionText.innerHTML = `Commissions: ${status}`;
+  }
+
+  function createRequest() {
+    let req = new XMLHttpRequest();
+    req.addEventListener('load', responseListener);
+    req.open('GET', 'https://trello.com/card/602c0c00f788320d4df57d97/status.json');
+    return req;
+  }
+
+  function 
 
   window.onload = function () {
     console.log(owo.join('\n'));
     console.log("what's this??");
     document.title = title;
 
-    main();
-    setInterval(main, 1000);
+    let req = createRequest();
+    req.send();
+
+    scrollTitle();
+    setInterval(scrollTitle, 1000);
   }
 })();
 
